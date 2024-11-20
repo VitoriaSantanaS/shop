bf96c5f08ee6ce3575b81da8cdd15539486aed93
VitóriaSantanaS <vitoriasantana0807@gmail.com>
2024-11-20 17:44:11 -0300

    inicio modulo animação

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -12,7 +12,8 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final passwordController = TextEditingController();
   final emailController = TextEditingController();
   final _formkey = GlobalKey<FormState>();
@@ -25,15 +26,49 @@ class _AuthFormState extends State<AuthForm> {
     'password': '',
   };
 
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
   bool _isLogin() => _authMode == AuthMode.Login;
   bool _isSignup() => _authMode == AuthMode.Signup;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+
+    _heightAnimation = Tween(
+      begin: const Size(double.infinity, 310),
+      end: const Size(double.infinity, 370),
+    ).animate(
+      CurvedAnimation(
+       parent: _controller!,
+       curve: Curves.linear,
+      )
+    );
+
+    _heightAnimation?.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   void _switchAuthMode() {
     setState(() {
       if (_isLogin()) {
         _authMode = AuthMode.Signup;
+        _controller?.forward();
       } else {
         _authMode = AuthMode.Login;
+        _controller?.reverse();
       }
     });
   }
@@ -91,7 +126,7 @@ class _AuthFormState extends State<AuthForm> {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: Container(
           padding: const EdgeInsets.all(16),
-          height: _isLogin() ? 310 : 370,
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 370),
           width: deviceSize.width * 0.75,
           child: Form(
               key: _formkey,
